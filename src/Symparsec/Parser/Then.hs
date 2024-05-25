05732d4c2f1f14723c057b272c4d8b3e24c2ea5f
Ben Orchard <thefirstmuffinman@gmail.com>
2024-05-25 03:10:03 +0100

    Parser/Then: fix end handling

--- src/Symparsec/Parser/Then.hs
+++ src/Symparsec/Parser/Then.hs
@@ -18,7 +18,7 @@ sThen
     -> SParser ssr srr ('PParser prCh prEnd s0r)
     -> SPThen  ssl srl ssr srr plCh plEnd s0l prCh prEnd s0r
 sThen (SParser plCh plEnd s0l) (SParser prCh prEnd s0r) =
-    SParser (sThenChSym plCh prCh s0r) (sThenEndSym plEnd prEnd) (SLeft s0l)
+    SParser (sThenChSym plCh prCh s0r) (sThenEndSym plEnd prEnd s0r) (SLeft s0l)
 
 instance
   -- Shame I can't use pl, pr in associated type synonyms! :(
@@ -49,8 +49,6 @@ type family pl :<*>: pr where
     'PParser plCh plEnd s0l :<*>: 'PParser prCh prEnd s0r =
         Then' plCh plEnd s0l prCh prEnd s0r
 
--- TODO thrown plEnd into ThenEndSym because otherwise we get reifying issues.
--- lmao
 type Then'
     :: ParserChSym  sl rl
     -> ParserEndSym sl rl
@@ -60,7 +58,7 @@ type Then'
     -> sr
     -> PParser (Either sl (rl, sr)) (rl, rr)
 type Then' plCh plEnd s0l prCh prEnd s0r =
-    'PParser (ThenChSym plCh prCh s0r) (ThenEndSym plEnd prEnd) (Left s0l)
+    'PParser (ThenChSym plCh prCh s0r) (ThenEndSym plEnd prEnd s0r) (Left s0l)
 
 type ThenCh
     :: ParserChSym sl rl
@@ -75,13 +73,23 @@ type family ThenCh plCh prCh s0r ch s where
 
 type family ThenChL prCh s0r ch resl where
     ThenChL prCh s0r ch (Cont sl) = Cont (Left  sl)
-    ThenChL prCh s0r ch (Done rl) = ThenChR rl (prCh @@ ch @@ s0r)
+    ThenChL prCh s0r ch (Done rl) =
+        -- 'Done' doesn't consume, so re-parse with the R parser.
+        ThenChR rl (prCh @@ ch @@ s0r)
     ThenChL prCh s0r ch (Err  el) = Err  (EThenChL el)
+
+type EThenChL el = EIn "Then(L)" el
+eThenChL :: SE el -> SE (EThenChL el)
+eThenChL el = withSingE el $ singE
 
 type family ThenChR rl resr where
     ThenChR rl (Cont sr) = Cont (Right '(rl, sr))
     ThenChR rl (Done rr) = Done '(rl, rr)
     ThenChR rl (Err  er) = Err  (EThenChR er)
+
+type EThenChR er = EIn "Then(R)" er
+eThenChR :: SE er -> SE (EThenChR er)
+eThenChR er = withSingE er $ singE
 
 sThenChR
     :: srl rl
@@ -127,40 +135,57 @@ type ThenChSym1
 data ThenChSym1 plCh prCh sr ch s
 type instance App (ThenChSym1 plCh prCh sr ch) s = ThenCh plCh prCh sr ch s
 
-type family ThenEnd prEnd s where
-    ThenEnd prEnd (Right '(rl, sr)) = ThenEnd' rl (prEnd @@ sr)
-    ThenEnd prEnd (Left sl)         = Left EThenEndL
+type family ThenEnd plEnd prEnd s0r s where
+    -- | EOT during R: call R end
+    ThenEnd plEnd prEnd s0r (Right '(rl, sr)) = ThenEndR rl    (prEnd @@ sr)
+    -- | EOT during L: call L end, pass R end
+    ThenEnd plEnd prEnd s0r (Left sl)         = ThenEndL prEnd s0r (plEnd @@ sl)
 
-type EThenEndL = EBase "Then" (Text "ended during left")
+type family ThenEndR rl res where
+    -- | EOT during R, R end succeeds: success
+    ThenEndR rl (Right rr) = Right '(rl, rr)
+    -- | EOT during R, R end fails: error
+    ThenEndR rl (Left  er) = Left  (EThenEndR er)
 
-type family ThenEnd' rl s where
-    ThenEnd' rl (Right rr) = Right '(rl, rr)
-    ThenEnd' rl (Left  er) = Left  (EThenChR er)
+type EThenEndR er = EIn "Then(R) end" er
+eThenEndR :: SE er -> SE (EThenEndR er)
+eThenEndR er = withSingE er $ singE
 
-type EThenChL el = EIn "Then(L)" el
-eThenChL :: SE el -> SE (EThenChL el)
-eThenChL el = withSingE el $ singE
+sThenEndR
+    :: srl rl
+    -> SResultEnd srr res
+    -> SResultEnd (STuple2 srl srr) (ThenEndR rl res)
+sThenEndR rl = \case
+  SRight rr -> SRight $ STuple2 rl rr
+  SLeft  er -> SLeft  $ eThenEndR er
 
-type EThenChR er = EIn "Then(R)" er
-eThenChR :: SE er -> SE (EThenChR er)
-eThenChR er = withSingE er $ singE
+type family ThenEndL prEnd s0r res where
+    -- | EOT during L, L end succeeds: call R end on initial R state
+    ThenEndL prEnd s0r (Right rl) = ThenEndR rl (prEnd @@ s0r)
+    -- | EOT during L, L end fails: error
+    ThenEndL prEnd s0r (Left  el) = Left  (EThenEndL el)
+
+type EThenEndL er = EIn "Then(L) end" er
+eThenEndL :: SE er -> SE (EThenEndL er)
+eThenEndL er = withSingE er $ singE
 
 sThenEndSym
     :: SParserEndSym ssl srl plEnd
     -> SParserEndSym ssr srr prEnd
+    -> ssr s0r
     -> SParserEndSym (SEither ssl (STuple2 srl ssr)) (STuple2 srl srr)
-        (ThenEndSym plEnd prEnd)
-sThenEndSym _plEnd prEnd = Lam $ \case
-  SRight (STuple2 rl sr) ->
-    case prEnd @@ sr of
-      SRight rr -> SRight $ STuple2 rl rr
-      SLeft  er -> SLeft  $ eThenChR er
-  SLeft  _sl -> SLeft singE
+        (ThenEndSym plEnd prEnd s0r)
+sThenEndSym plEnd prEnd s0r = Lam $ \case
+  SRight (STuple2 rl sr) -> sThenEndR rl (prEnd @@ sr)
+  SLeft  sl ->
+    case plEnd @@ sl of
+      SRight rl -> sThenEndR rl (prEnd @@ s0r)
+      SLeft  el -> SLeft $ eThenEndL el
 
--- TODO plEnd is unused. need it for reifying to work. lmao
 type ThenEndSym
     :: ParserEndSym sl rl
     -> ParserEndSym sr rr
+    -> sr
     -> ParserEndSym (Either sl (rl, sr)) (rl, rr)
-data ThenEndSym plEnd prEnd s
-type instance App (ThenEndSym plEnd prEnd) s = ThenEnd prEnd s
+data ThenEndSym plEnd prEnd s0r s
+type instance App (ThenEndSym plEnd prEnd s0r) s = ThenEnd plEnd prEnd s0r s
