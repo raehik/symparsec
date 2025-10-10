78238d3d9286f0a5cb9837aca10b6dc352c580a3
Ben Orchard <thefirstmuffinman@gmail.com>
2025-10-10 14:26:06 +0100

    Example.Expr: improve errors
    Does mean some nasty state passing. Oh well.

--- src/Symparsec/Example/Expr.hs
+++ src/Symparsec/Example/Expr.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE UndecidableInstances #-}
 
+-- | An example Symparsec parser for a basic expression tree.
+
 module Symparsec.Example.Expr where
 
 import Symparsec.Parser.Common
@@ -8,54 +10,86 @@ import Symparsec.Parser.Natural
 import Symparsec.Parser.Natural.Digits
 import Symparsec.Parser.While ( type While )
 import Symparsec.Parser.While.Predicates ( type IsDecDigitSym )
+import GHC.TypeNats qualified as TypeNats
+
+-- | A basic expression tree, polymorphic over a single literal type.
+data Expr a
+  = EBOp BOp (Expr a) (Expr a)
+  | ELit a
+
+-- | A binary operator.
+data BOp = Add | Sub | Mul | Div
+
+-- | Evaluate an 'Expr' of 'Natural's on the type level.
+--
+-- Naive, doesn't attempt to tail-call recurse.
+type Eval :: Expr Natural -> Natural
+type family Eval expr where
+    Eval (EBOp bop l r) = EvalBOp bop (Eval l) (Eval r)
+    Eval (ELit n) = n
+
+type EvalBOp :: BOp -> Natural -> Natural -> Natural
+type family EvalBOp bop l r where
+    EvalBOp Add l r = l + r
+    EvalBOp Sub l r = l - r
+    EvalBOp Mul l r = l * r
+    EvalBOp Div l r = l `TypeNats.Div` r
+
+-- | 'Expr' tokens.
+data ExprTok a
+  = TokBOp BOp
+  | TokLit a
 
 type PExpr :: PParser (Expr Natural)
 data PExpr s
-type instance App PExpr s = PExprNext '[] '[] (UnconsState s)
+type instance App PExpr s = PExprNext s '[] '[] (UnconsState s)
 type PExprNext
-    :: [BOp]
+    :: PState
+    -> [BOp]
     -> [Expr Natural]
     -> (Maybe Char, PState)
     -> PReply (Expr Natural)
-type family PExprNext ops exprs s where
-    PExprNext ops exprs '(Just ch, s) =
-        PExprCh s ops exprs ch
-    PExprNext ops exprs '(Nothing, s) = PExprEnd s ops exprs
+type family PExprNext sPrev ops exprs s where
+    PExprNext sPrev ops exprs '(Just ch, s) =
+        PExprCh sPrev s ops exprs ch
+    PExprNext sPrev ops exprs '(Nothing, s) = PExprEnd sPrev s ops exprs
 
-type family PExprEnd s ops exprs where
-    PExprEnd s (op:ops) exprs      = PExprEndPopOp s ops exprs op
-    PExprEnd s '[]      (expr:'[]) = 'Reply (OK expr) s
-    PExprEnd s '[]      _          =
-        'Reply (Err (Error1 "badly formed expression")) s
+type family PExprEnd sPrev s ops exprs where
+    PExprEnd sPrev s (op:ops) exprs      = PExprEndPopOp sPrev s ops exprs op
+    PExprEnd sPrev s '[]      (expr:'[]) = 'Reply (OK expr) s
+    PExprEnd sPrev s '[]      _          =
+        'Reply (Err (Error1 "badly formed expression")) sPrev
 
-type family PExprEndPopOp s ops exprs op where
-    PExprEndPopOp s ops (r:l:exprs) bop =
-        PExprEnd s ops (EBOp bop l r : exprs)
+type family PExprEndPopOp sPrev s ops exprs op where
+    PExprEndPopOp sPrev s ops (r:l:exprs) bop =
+        PExprEnd sPrev s ops (EBOp bop l r : exprs)
+    PExprEndPopOp sPrev s ops exprs bop =
+        'Reply (Err (Error1 "badly formed expression")) sPrev
 
-type family PExprCh s ops exprs ch where
-    PExprCh s ops exprs ' ' = PExprNext ops exprs (UnconsState s)
-    PExprCh s ops exprs ch  = PExprELit s ops exprs ch (ParseDigitDecSym @@ ch)
+type family PExprCh sPrev s ops exprs ch where
+    PExprCh sPrev s ops exprs ' ' = PExprNext s ops exprs (UnconsState s)
+    PExprCh sPrev s ops exprs ch  = PExprELit sPrev s ops exprs ch (ParseDigitDecSym @@ ch)
 
-type family PExprELit s ops exprs ch mDigit where
-    PExprELit s ops exprs _ch (Just digit) =
+type family PExprELit sPrev s ops exprs ch mDigit where
+    PExprELit sPrev s ops exprs _ch (Just digit) =
         PExprELitEnd ops exprs
             (While IsDecDigitSym (NatBase1 10 ParseDigitDecSym digit) @@ s)
-    PExprELit s ops exprs  ch Nothing      =
-        PExprEBOp s ops exprs ch (PExprEBOpOpCh ch)
+    PExprELit sPrev s ops exprs  ch Nothing      =
+        PExprEBOp sPrev s ops exprs ch (PExprEBOpOpCh ch)
 
 type family PExprELitEnd ops exprs res where
     PExprELitEnd ops exprs ('Reply (OK  n) s) =
-        PExprNext ops (ELit n : exprs) (UnconsState s)
+        PExprNext s ops (ELit n : exprs) (UnconsState s)
     PExprELitEnd ops exprs ('Reply (Err e) s) =
-        'Reply (Err e) s
+        -- The digit parser we're wrapping shouldn't ever fail, due to how
+        -- 'While' works, and that we've already handled the 0-length case.
+        Impossible
 
-type family PExprEBOp s ops exprs ch mbop where
-    PExprEBOp s ops exprs ch (Just bop) =
-        PExprEBOp' s bop (BOpPrec bop) exprs ops
-    PExprEBOp s ops exprs ch Nothing =
-        -- TODO we're 1 char ahead here, since we've already consumed the op...
-        -- oops.
-        'Reply (Err (Error1 "bad expression, expected digit or operator")) s
+type family PExprEBOp sPrev s ops exprs ch mbop where
+    PExprEBOp sPrev s ops exprs ch (Just bop) =
+        PExprEBOp' sPrev s bop (BOpPrec bop) exprs ops
+    PExprEBOp sPrev s ops exprs ch Nothing =
+        'Reply (Err (Error1 "bad expression, expected digit or operator")) sPrev
 
 type family PExprEBOpOpCh ch where
     PExprEBOpOpCh '+' = Just Add
@@ -64,18 +98,19 @@ type family PExprEBOpOpCh ch where
     PExprEBOpOpCh '/' = Just Div
     PExprEBOpOpCh _   = Nothing
 
-type family PExprEBOp' s op prec exprs ops where
-    PExprEBOp' s op prec exprs (opPrev:ops) =
+type family PExprEBOp' sPrev s op prec exprs ops where
+    PExprEBOp' sPrev s op prec exprs (opPrev:ops) =
         IfNatLte prec (BOpPrec opPrev)
-            (PExprEBOpPop s op prec opPrev ops exprs)
-            (PExprNext (op:opPrev:ops) exprs (UnconsState s))
-    PExprEBOp' s op prec exprs '[]          =
-        PExprNext '[op] exprs (UnconsState s)
+            (PExprEBOpPop sPrev s op prec opPrev ops exprs)
+            (PExprNext sPrev (op:opPrev:ops) exprs (UnconsState s))
+    PExprEBOp' sPrev s op prec exprs '[]          =
+        PExprNext s '[op] exprs (UnconsState s)
 
-type family PExprEBOpPop s op prec opPrev ops exprs where
-    PExprEBOpPop s op prec opPrev ops (r:l:exprs) =
-        PExprEBOp' s op prec (EBOp opPrev l r : exprs) ops
-    -- TODO add errorful case
+type family PExprEBOpPop sPrev s op prec opPrev ops exprs where
+    PExprEBOpPop sPrev s op prec opPrev ops (r:l:exprs) =
+        PExprEBOp' sPrev s op prec (EBOp opPrev l r : exprs) ops
+    PExprEBOpPop sPrev s op prec opPrev ops exprs =
+        'Reply (Err (Error1 "badly formed expression")) sPrev
 
 type BOpPrec :: BOp -> Natural
 type family BOpPrec bop where
@@ -83,17 +118,6 @@ type family BOpPrec bop where
     BOpPrec Sub = 2
     BOpPrec Mul = 3
     BOpPrec Div = 3
-
-data Expr a
-  = EBOp BOp (Expr a) (Expr a)
-  | ELit a
-
-data BOp = Add | Sub | Mul | Div
-
--- | 'Expr' tokens.
-data ExprTok a
-  = TokBOp BOp
-  | TokLit a
 
 {-
 import GHC.TypeError qualified as TE
