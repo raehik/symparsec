5af9774df59e432ac84594efab5e5f3914a30e42
Ben Orchard <thefirstmuffinman@gmail.com>
2025-10-23 17:11:23 +0100

    fix tests

--- test/Main.hs
+++ test/Main.hs
@@ -12,10 +12,10 @@ type CstrX_Y = LiftA2 (Con2 '(,))
     (Literal "_"    *> Isolate 2 NatHex)
 
 spec :: Expect
-    '[ Run (Literal "raehik") "raehik" `Is` Right '( '(), "")
-     , Run (Literal "raeh") "raehraeh" `Is` Right '( '(), "raeh")
-     , Run (Skip 3 *> Literal "HI") "...HI" `Is` Right '( '(), "")
-     , Run (Literal "0x" *> NatHex) "0xfF" `Is` Right '( 255, "")
-     , Run CstrX_Y "Cstr12_AB" `Is` Right '( '(12, 0xAB), "")
+    '[ Run' (Literal "raehik") "raehik" `Is` Right '( '(), "")
+     , Run' (Literal "raeh") "raehraeh" `Is` Right '( '(), "raeh")
+     , Run' (Skip 3 *> Literal "HI") "...HI" `Is` Right '( '(), "")
+     , Run' (Literal "0x" *> NatHex) "0xfF" `Is` Right '( 255, "")
+     , Run' CstrX_Y "Cstr12_AB" `Is` Right '( '(12, 0xAB), "")
      ]
 spec = Valid
