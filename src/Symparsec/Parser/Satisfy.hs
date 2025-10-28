c76e50afbaa4cd363e90d045a6040aa2cc908c13
Ben Orchard <thefirstmuffinman@gmail.com>
2025-10-28 21:41:24 +0000

    Parser: +Satisfy

---
+++ src/Symparsec/Parser/Satisfy.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE UndecidableInstances #-}
+
+module Symparsec.Parser.Satisfy ( type Satisfy, type OneOf ) where
+
+import Symparsec.Parser.Common
+
+-- may also be defined using @Token@
+type Satisfy :: (Char ~> Bool) -> PParser Char
+data Satisfy chPred ps
+type instance App (Satisfy chPred) ps = SatisfyStart chPred ps (UnconsState ps)
+
+type family SatisfyStart chPred psPrev mps where
+    SatisfyStart chPred psPrev '(Just ch, ps) = SatisfyValidate psPrev ps ch (chPred @@ ch)
+    SatisfyStart chPred psPrev '(Nothing, ps) =
+        'Reply (Err (Error1 "expected at least 1 char")) ps
+
+type family SatisfyValidate psPrev ps ch res where
+    SatisfyValidate psPrev ps ch True  = 'Reply (OK ch) ps
+    SatisfyValidate psPrev ps ch False =
+        'Reply (Err (Error1 "satisfy: char failed predicate")) psPrev
+
+type OneOf :: [Char] -> PParser Char
+type OneOf chs = Satisfy (ElemSym chs)
+
+-- TODO put in singleraeh
+type Elem :: a -> [a] -> Bool
+type family Elem a as where
+    Elem _ '[]    = False
+    Elem a (a:_)  = True
+    Elem a (_:as) = Elem a as
+
+-- NOTE: flipped from "normal" Elem
+type ElemSym :: [a] -> a ~> Bool
+data ElemSym as a
+type instance App (ElemSym as) a = Elem a as
